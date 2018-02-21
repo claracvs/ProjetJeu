aa21c42d7fba8dfbc3599cf2e064eefae99b63d1
claracvs <35968447+claracvs@users.noreply.github.com>
2018-02-21 12:19:51 +0100

    Add files via upload

--- 2I013/Awele/main.py
+++ 2I013/Awele/main.py
@@ -4,20 +4,11 @@ sys.path.append("..")
 import game
 game.game=awele
 sys.path.append("./Joueurs")
+import joueurMiniMax
 import joueur_aleatoire
-import awele
-import sys
-sys.path.append("..")
-import game
-game.game=awele
-sys.path.append("./Joueurs")
-import joueur_humain
-import joueur_premiercoup
 import joueur_derniercoup
-import joueur_aleatoire
-
-joueur1=joueur_premiercoup #Contient le module du joueur 1
-joueur2=joueur_premiercoup #Contient le module du joueur 2
+game.joueur1=joueurMiniMax
+game.joueur2=joueur_aleatoire
 
 def joue ():
     """void -> nat
@@ -25,30 +16,26 @@ def joue ():
     jeu = game.initialiseJeu ()
  
     while not game.finJeu(jeu) : 
-        #game.affiche(jeu)
-        print "joueur : {}".format(game.getJoueur(jeu))
         coup = game.saisieCoup(jeu)
-        print "coup joue : {}".format(coup)
         game.joueCoup(jeu, coup)
-        print "score : {}".format(game.getScores(jeu))
-        #game.affiche(jeu)
     return game.getGagnant (jeu)
     
-victoires = []
-for i in range (10) : 
+victoires = [0,0]
+for i in range (50) : 
     gagnant = joue()
-    victoires[gagnant]+=1
+    if gagnant :
+        victoires[gagnant-1]+=1
 
 print str(victoires)
-game.affiche(jeu)
 
 joueur = game.joueur2
 game.joueur2 = game.joueur1
 game.joueur1 = joueur
 
-for i in range (10) : 
+for i in range (50) : 
     g=joue()
-    victoires[g%2+1]+=1
+    if g :
+        victoires[g%2]+=1
 
 print str(victoires)
 
