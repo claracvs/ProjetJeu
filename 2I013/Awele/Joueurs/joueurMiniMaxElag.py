ebc232767235577f633f2c79284d7687ac60c5ee
claracvs <35968447+claracvs@users.noreply.github.com>
2018-03-07 10:07:50 +0100

    Add files via upload

--- 2I013/Awele/Joueurs/joueurMiniMaxElag.py
+++ 2I013/Awele/Joueurs/joueurMiniMaxElag.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 21 08:59:54 2018
+Created on Wed Feb 28 09:26:54 2018
 
 @author: 3671207
 """
 
-#ne sait pas jouer en joueur 2
-import game
+import game 
+
 
 moi = None
 PMAX = 2
@@ -15,29 +15,23 @@ def saisieCoup(jeu) :
     global moi
     moi=game.getJoueur(jeu)
     coup=decision(jeu,game.getCoupsValides(jeu))
-    
     return coup
     
 def decision (jeu, listeCoups) : 
     """ jeu*[coup]-> pair[coup, score]
     choisit le coup à jouer dans une liste de coup possibles
     """
-    #print(str(listeCoups))    
     coupAjouer = None
     scoremax = -10000000000
-    s=""
     for coup in listeCoups :
-        score_estime = estimation (jeu, coup, 1)
-        s+=","+str(score_estime)
+        score_estime = estimation (jeu, coup, 1, -10000000,10000000)
         if score_estime > scoremax : 
             coupAjouer = coup
             scoremax=score_estime
-    #print(s)
-    #print(coupAjouer)
     return coupAjouer
     
     
-def estimation (jeu, coup, profondeur) : 
+def estimation (jeu, coup, profondeur, alpha, beta) : 
     copie = game.getCopieJeu(jeu)
     game.joueCoup (copie, coup)    
     if game.finJeu(copie) : 
@@ -52,66 +46,30 @@ def estimation (jeu, coup, profondeur) :
     if profondeur == PMAX :
         return evaluation (copie)
         
-    listeCoups = game.getCoupsValides (copie)        
-    if profondeur%2 ==0:
-        scoremax=-10000
+    listeCoups = game.getCoupsValides (copie)  
+   
+    if profondeur%2 ==0:  #noeud max
+        scoremax=-10000000
         for c in listeCoups : 
-            score_estime = estimation(copie, c, profondeur+1)
-
-            if  score_estime > scoremax :
+            score_estime = estimation(copie, c, profondeur+1, max(alpha, scoremax), beta)
+            if  score_estime >= beta :
+                return 10000000  
+            if score_estime>scoremax : 
                 scoremax=score_estime
         return scoremax
 
-    if profondeur%2==1:
-        scoremin=10000
+    if profondeur%2==1:#noeud min
+        scoremin=10000000
         for c in listeCoups : 
-            score_estime = estimation(copie, c, profondeur+1)
-            if  score_estime < scoremin :
+            score_estime = estimation(copie, c, profondeur+1, alpha, min(beta, scoremin))
+            if  score_estime <= alpha :
+                return -10000000
+            if score_estime<scoremin:
                 scoremin=score_estime
         return scoremin
-        
+
+            
 def evaluation (jeu) : 
     """ jeu -> int 
     """
     return game.getScore(jeu,moi)-game.getScore(jeu, moi%2+1)
-
-    
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
